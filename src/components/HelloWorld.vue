178769605c51472d36164529441ed3b6c2a8447f
ljutaev <ljutaev@gmail.com>
2019-11-20 13:29:57 +0200

    Components & props

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Welcome to the forum</h1>
+    <ThreadList :threads="objec"/>
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data'
+  import ThreadList from './ThreadList'
+  export default {
+    name: 'HelloWorld',
+    components: {
+      ThreadList
+    },
+    data () {
+      return {
+        threads: Object.values(sourceData.threads),
+        posts: sourceData.posts,
+        users: sourceData.users,
+      }
+    }
+  }
+</script>
